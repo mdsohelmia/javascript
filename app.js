a69360555c9491dcb43f5077644b03b76a8a5fe7
Sohel Mia <sohelcse1999@gmail.com>
2020-03-26 20:56:46 +0600

    template literals

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-alert('hie');
+
+// template litears example
+
+//old javacript example;
+
+// var firstname = 'Sohel';
+
+// var lastname = 'Mia';
+
+// var fullname = firstname+' ' + lastname;
+
+// console.log(fullname);
+
+
+/**
+ * `template literals`
+ */
+
+let firstname = 'Sohel';
+
+let lastname = 'Mia';
+
+let fullname = `${firstname} ${lastname}`;
+
+console.log(fullname);
+
+let num =10;
+
+let num2 =20;
+
+
+const sumation = `Num and num2 is sumation ${num+num} `;
+
+
+console.log(sumation);
+
+
+let  example = "Hello\n" + "world";
+console.log(example);
+
+document.getElementById('example').innerText=example;
